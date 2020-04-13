b577effff7413569a36ac8d7a5469fadeb8d8127
Mohammad Hasani <thebrodmann@protonmail.com>
2020-04-14 03:14:46 +0430

    style(prime): format via Brittany

--- prime/Main.hs
+++ prime/Main.hs
@@ -20,30 +20,30 @@ import           Control.Concurrent
 data Tree a = Tree (Tree a) a (Tree a)
 
 instance Functor Tree where
-    fmap f (Tree l m r) = Tree (fmap f l) (f m) (fmap f r)
+  fmap f (Tree l m r) = Tree (fmap f l) (f m) (fmap f r)
 
 index :: Integral a => Tree b -> a -> b
 index (Tree _ m _) 0 = m
 index (Tree l _ r) n = case (n - 1) `divMod` 2 of
-    (q,0) -> index l q
-    (q,1) -> index r q
-    _     -> error "not reachable"
+  (q, 0) -> index l q
+  (q, 1) -> index r q
+  _      -> error "not reachable"
 
 nats :: Integral a => Tree a
 nats = go 0 1
-    where
-        go !n !s = Tree (go l s') n (go r s')
-            where
-                l = n + s
-                r = l + s
-                s' = s * 2
+ where
+  go !n !s = Tree (go l s') n (go r s')
+   where
+    l  = n + s
+    r  = l + s
+    s' = s * 2
 
 isqrt :: Integral a => a -> a
 isqrt = floor @Double . sqrt . fromIntegral
 {-# INLINE isqrt #-}
 
 -- | Main logic
-  
+
 factors :: Integral a => a -> [a]
 factors n = [ x | x <- [3, 5 .. isqrt n], n `mod` x == 0 ]
 
@@ -66,7 +66,7 @@ isPrimeMemo = index isPrimeTree
 output :: Integral a => Either String a -> Text
 output (Left _) = error "Parser error: not number input."
 output (Right x) | isPrimeMemo x = "1"
-                 | otherwise = "0"
+                 | otherwise     = "0"
 
 parseIntegral :: Text -> Either String Int32
 parseIntegral = parseOnly decimal
