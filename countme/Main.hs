4a1b7077361f8f9e8e30b88ee556c5fd1e6f242e
Mohammad Hasani <thebrodmann@protonmail.com>
2020-04-28 16:40:15 +0430

    perf(countme): parse body in lazy context

--- countme/Main.hs
+++ countme/Main.hs
@@ -14,18 +14,18 @@ import qualified Data.ByteString.Lazy.Char8    as LBS8
 
 type CounterRef = IORef Int
 
-response :: Int -> Response
-response = responseLBS status200 [] . encode
+modify :: LBS8.ByteString -> Int -> Int
+modify body = case LBS8.readInt body of
+  Nothing -> error "!!!"
+  Just (x, _) -> (+ x)
+{-# INLINE modify #-}
 
 application :: CounterRef -> Application
 application ref request@Request { requestMethod = "POST" } respond = do
-  body <- LBS8.readInt <$> strictRequestBody request
-  case body of
-    Nothing     -> error "!!!"
-    Just (x, _) -> do
-      modifyIORef ref (+ x)
-      respond $ responseLBS status200 [] mempty
-application ref _ respond = respond . response =<< readIORef ref
+  modifyIORef ref . modify =<< strictRequestBody request
+  respond $ responseLBS status200 [] mempty
+application ref _ respond =
+  respond . responseLBS status200 [] . encode =<< readIORef ref
 
 main :: IO ()
 main = run 80 <$> application =<< newIORef 0
