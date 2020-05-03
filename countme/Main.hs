560ed1e03f10bbcc30aa25c69b544eb9925d3a66
Mohammad Hasani <thebrodmann@protonmail.com>
2020-05-03 14:08:13 +0430

    revert(countme): separate number and times ref

--- countme/Main.hs
+++ countme/Main.hs
@@ -12,38 +12,17 @@ import           Network.HTTP.Types
 import           Data.Aeson                     ( encode )
 import qualified Data.ByteString.Lazy.Char8    as LBS8
 
-type NumberRef = IORef (Maybe Int)
-type TimesRef = IORef Int
+type CounterRef = IORef Int
 
-whenNothing_ :: Applicative f => Maybe a -> f () -> f ()
-whenNothing_ Nothing m = m
-whenNothing_ _       _ = pure ()
-{-# INLINE whenNothing_ #-}
-
-whenNothingM_ :: Monad m => m (Maybe a) -> m () -> m ()
-whenNothingM_ mm action = mm >>= \m -> whenNothing_ m action
-{-# INLINE whenNothingM_ #-}
-
-modify :: Int -> (Int, ())
-modify x = (x + 1, ())
-{-# INLINE modify #-}
-
-application :: NumberRef -> TimesRef -> Application
-application numberRef timesRef request@Request { requestMethod = "POST" } respond = do
-  whenNothingM_ (readIORef numberRef) $ do
-    body <- LBS8.readInt <$> strictRequestBody request
-    case body of
-      Nothing     -> error "!!!"
-      Just (x, _) -> atomicWriteIORef numberRef $ Just x
-  atomicModifyIORef' timesRef modify
+application :: CounterRef -> Application
+application ref request@Request { requestMethod = "POST" } respond = do
+  body <- LBS8.readInt <$> strictRequestBody request
+  case body of
+    Nothing     -> error "!!!"
+    Just (x, _) -> atomicModifyIORef' ref $ \counter -> (counter + x, ())
   respond $ responseLBS status200 [] mempty
-application numberRef timesRef _ respond = do
-  number <- maybe 0 id <$> readIORef numberRef
-  times <- readIORef timesRef
-  respond . responseLBS status200 [] $ encode (number * times)
+application ref _ respond =
+  respond . responseLBS status200 [] . encode =<< readIORef ref
 
 main :: IO ()
-main = do
-  numberRef <- newIORef Nothing
-  timesRef <- newIORef 0
-  run 80 $ application numberRef timesRef
+main = run 80 <$> application =<< newIORef 0
